d763ec8641310f79475d4f24214b4723495acd14
bora.K <147504785+surely07@users.noreply.github.com>
2023-12-26 10:16:27 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,11 @@
+function solution(t, p) {
+  let answer = 0;
+
+  for (let i = 0; i <= t.length - p.length; i++) {
+    const subStr = t.substring(i, i + p.length);
+    if (parseInt(subStr) <= parseInt(p)) {
+      answer = answer + 1;
+    }
+  }
+  return answer;
+}
